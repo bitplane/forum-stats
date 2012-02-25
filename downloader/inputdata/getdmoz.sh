eade6dc96a993d67723a6de12c51e32000ac95ee
Gaz Davidson <gaz@bitplane.net>
2012-02-25 09:46:56 +0000

    Added a script to download and parse the open directory in search of forums
    It streams the content from ODP so doesn't need ~2GiB, but must start again
    if it gets disconnected. I like disk space, and pipes. Specially the pipes.

---
+++ downloader/inputdata/getdmoz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'Downloading list of sites from The Open Directory,'
+echo 'this may take a while. Keep an eye on dmoz.txt, it'
+echo 'should have about 10,000 rows when complete.'
+
+URL=http://rdf.dmoz.org/rdf/content.rdf.u8.gz
+
+# strip quotes, replace &amp;, replace &quot;
+PARSE='s/.*\"\(.*.*\)\".*/\1/;s/\&amp;/\&/g;s/\&quot;/\"/g'
+
+# download     | extract | fast filter       | slow filter   | parse      > save
+wget -qO- $URL | zcat    | grep ExternalPage | grep -i forum | sed $PARSE > dmoz.txt
+
+if [ $? == 0 ]
+then
+    echo 'All done!'
+else
+    echo 'Ugh, something went wrong :('
+fi
+
